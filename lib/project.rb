f4dd7e412acbc718a008019a42293873dfca9c79
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-29 16:02:24 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,12 +8,13 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    self.add_to_backer(backer)
+    backer.back_projects(self) unless backer.backed_projects.include?(self)
+    # self.add_to_backer(backer)
   end
   
-  def add_to_backer(backer)
-    backer.projects.include?(self)
-    backer.add_to_project(self)
-  end
+  # def add_to_backer(backer)
+  #   backer.projects.include?(self)
+  #   backer.add_to_project(self)
+  # end
   
 end
